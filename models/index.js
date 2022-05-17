a12a9a9b31dd4a5fd5de2bc2b2d5c8b1752f3a82
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-05-17 15:07:18 -0400

    trying to get heroku to update with latest Vote.js change

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
-//reverse(one-to-one relationship): a post only belongs to one user
+//reverse(one-to-one relationship): a post only belongs to one user//
 Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
