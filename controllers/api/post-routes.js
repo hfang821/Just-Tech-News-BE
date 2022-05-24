26c2ab2e90600946cbb4588b360de7909ddd37bc
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-05-23 20:19:09 -0400

    fixing the bugs in the edit-post.handlebars (syntax error) and a bug in edit-post.js (syntax error)
    the website is now running at its intentions

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -136,23 +136,24 @@ router.put('/:id', withAuth,(req, res)=>{
 });
 
 router.delete('/:id', withAuth, (req, res) => {
+    console.log('id', req.params.id);
     Post.destroy({
-        where: {
-            id: req.params.id
-        }
+      where: {
+        id: req.params.id
+      }
     })
-    .then(dbPostData=>{
-        if(!dbPostData) {
-            res.status(404).json({message:'No post found!'});
-            return;
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
         }
         res.json(dbPostData);
-    })
-    .catch(err => {
+      })
+      .catch(err => {
         console.log(err);
         res.status(500).json(err);
-    });
-});
+      });
+  });
 
 
 
